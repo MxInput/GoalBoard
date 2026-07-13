5f0becd515c3a81a3ae10e854c2c8c1001ef9452
MxInput <mxinput@protonmail.com>
2026-07-13 04:48:34 -0400

    user can initialize goals

--- nodes/draw.gd
+++ nodes/draw.gd
@@ -5,16 +5,6 @@ var texture_size : Vector2i = Vector2i(512, 512);
 
 @export var source_img : Texture2D;
 
-@export var text_box : TextEdit;
-@export var text_count : Label;
-var current_text := "";
-var cursor_line := 0;
-var cursor_col := 0;
-
-var max_text_length := 300;
-
-@export var create_button : TextureButton;
-
 func _ready() -> void:
 	drawable_texture.setup(texture_size.x, texture_size.y, DrawableTexture2D.DRAWABLE_FORMAT_RGBA8, Color.WHITE)
 
@@ -23,26 +13,3 @@ func _process(delta: float) -> void:
 		var local_pos = get_local_mouse_position()
 		
 		drawable_texture.blit_rect(Rect2i(local_pos.x, local_pos.y, 8, 8), source_img, Color.RED)
-
-
-func _on_text_edit_text_changed() -> void:
-	var new_text := text_box.text;
-	
-	if (text_box.text.length() > max_text_length):
-		text_box.text = current_text;
-		
-		text_box.set_caret_column(cursor_col);
-		text_box.set_caret_line(cursor_line);
-	
-	current_text = text_box.text;
-	cursor_col = text_box.get_caret_column();
-	cursor_line = text_box.get_caret_line();
-
-	text_count.text = str(text_box.text.length()) + "/300"
-
-
-func _on_create_button_down() -> void:
-	if (text_box.text.length() > 0):
-		print("saving")
-	else:
-		print("enter your goals")
---
+++ nodes/goal_creation.gd
@@ -0,0 +1,59 @@
+extends Node
+
+@export var desc_box : TextEdit;
+@export var desc_count : Label;
+var current_text := "";
+var cursor_line := 0;
+var cursor_col := 0;
+
+var max_text_length := 300;
+
+@export var create_button : TextureButton;
+
+@export var identifier_box : LineEdit;
+@export var identifier_count : Label;
+
+@export var v_box : VBoxContainer;
+@export var temp_goal : PackedScene;
+
+func _on_text_edit_text_changed() -> void:
+	var new_text := desc_box.text;
+	
+	if (desc_box.text.length() > max_text_length):
+		desc_box.text = current_text;
+		
+		desc_box.set_caret_column(cursor_col);
+		desc_box.set_caret_line(cursor_line);
+	
+	current_text = desc_box.text;
+	cursor_col = desc_box.get_caret_column();
+	cursor_line = desc_box.get_caret_line();
+
+	desc_count.text = str(desc_box.text.length()) + "/300"
+
+
+func _on_create_button_down() -> void:
+	if (desc_box.text.length() > 0):
+		var desc_string = desc_box.text;
+		var identifier_string = identifier_box.text;
+		
+		desc_box.text = "";
+		desc_count.text = "0/300";
+		
+		identifier_box.text = "";
+		identifier_count.text = "0/300";
+		
+		var new_goal := temp_goal.instantiate();
+		v_box.add_child(new_goal);
+		new_goal.find_child("Identifier").text = identifier_string;
+		new_goal.find_child("Description").get_child(0).text = desc_string;
+		
+		var found_date = Time.get_date_dict_from_system();
+		new_goal.find_child("Date").text = str(found_date["month"]) + "/" + str(found_date["day"]) + "/" + str(found_date["year"]);
+		
+	else:
+		print("enter your goals")
+
+
+func _on_identifier_text_changed(new_text: String) -> void:
+	identifier_count.text = str(identifier_box.text.length()) + "/" + str(identifier_box.max_length);
