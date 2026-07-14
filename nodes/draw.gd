c331e04db35337a96fcf61185dab4285f8445662
MxInput <mxinput@protonmail.com>
2026-07-13 22:37:43 -0400

    copying drawing onto board

--- nodes/draw.gd
+++ nodes/draw.gd
@@ -7,21 +7,42 @@ var texture_size : Vector2i = Vector2i(512, 512);
 
 @export var canvas_layer : CanvasLayer;
 
+@export var camera : Camera2D;
+
+var completed = false;
+
+var board;
+
 func disappear_goal_menu() -> void:
 	for control_obj in canvas_layer.get_children():
 		if (control_obj.name != "Background" && control_obj.name != "DrawingTab"):
 			control_obj.visible = false;
 
 func _ready() -> void:
-	drawable_texture.setup(texture_size.x, texture_size.y, DrawableTexture2D.DRAWABLE_FORMAT_RGBA8, Color.WHITE)
+	drawable_texture.setup(texture_size.x, texture_size.y, DrawableTexture2D.DRAWABLE_FORMAT_RGBA8);
 
 func _process(_delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var local_pos = get_local_mouse_position()
 		
-		drawable_texture.blit_rect(Rect2i(local_pos.x, local_pos.y, 8, 8), source_img, Color.RED)
+		drawable_texture.blit_rect(Rect2i(local_pos.x, local_pos.y, 8, 8), source_img, Color.BLACK)
 
 func start_drawing(unfinished_goal : UnfinishedGoal) -> void:
 	get_parent().visible = true;
 	
 	disappear_goal_menu();
+
+func _on_clear_button_down() -> void:
+	drawable_texture.setup(texture_size.x, texture_size.y, DrawableTexture2D.DRAWABLE_FORMAT_RGBA8);
+
+
+func _on_finish_button_down() -> void:
+	var container = get_node("/root/Menu/Container");
+	container.visible = false;
+	canvas_layer.visible = false;
+	
+	board = preload("res://nodes/board.tscn").instantiate();
+	get_tree().root.add_child(board);
+	board.find_child("Camera2D").make_current();
+	board.find_child("Current").texture = ImageTexture.create_from_image(drawable_texture.get_image());
+	drawable_texture.setup(texture_size.x, texture_size.y, DrawableTexture2D.DRAWABLE_FORMAT_RGBA8);
--- nodes/goal_draw.gd
+++ nodes/goal_draw.gd
@@ -5,7 +5,7 @@ var unfinished_goal : UnfinishedGoal;
 signal activate_draw(selected_go);
 
 func _ready() -> void:
-	var canvas_layer = get_node("/root/Menu/CanvasLayer");
+	var canvas_layer = get_node("/root/Menu/Container/CanvasLayer");
 	var drawHandler = canvas_layer.find_child("DrawingTab");
 	var drawing = drawHandler.find_child("Drawing");
 	
